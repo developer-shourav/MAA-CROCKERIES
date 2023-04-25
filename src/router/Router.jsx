fa3ec80cae33e531541b61b8e4c8800fdb0ecf40
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-25 21:43:02 +0600

    home component added and all router setup complete

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,40 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+import Products from "../pages/Products/Products";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main> </Main>,
+      children:[
+        {
+          path:'/',
+          element:<Home> </Home>,
+        },
+
+        {
+          path:'/home',
+          element:<Home> </Home>,
+        },
+
+        {
+          path:'/products',
+          element:<Products></Products>,
+        },
+
+        {
+          path:'/login',
+          element:<Login> </Login>,
+        },
+
+        {
+          path:'/register',
+          element:<Register></Register>,
+        }
+      ]
     },
   ]);
 
