c920d623eab14d32d8d101f3d2d6c247e1da0dc8
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-25 22:31:58 +0600

    slider added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import BannerSlider from '../components/BannerSlider';
 
 const Home = () => {
     return (
         <div>
             <h2>This is Home </h2>
+            <BannerSlider> </BannerSlider>
         </div>
     );
 };
--- src/pages/components/BannerSlider.jsx
+++ src/pages/components/BannerSlider.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
 
 const BannerSlider = () => {
     return (
         <>
-            
+          <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        
+      </Swiper>  
         </>
     );
 };
