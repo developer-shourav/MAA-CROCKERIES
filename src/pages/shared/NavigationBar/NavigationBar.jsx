3e58328c28230bea3f10d91eeeebdad4a9797f86
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-25 13:51:28 +0600

    rsponsive nav bar complete

--- src/pages/shared/NavigationBar/NavigationBar.jsx
+++ src/pages/shared/NavigationBar/NavigationBar.jsx
@@ -1,29 +1,92 @@
-import React from 'react';
-import Logo from '../../../assets/web-logo.png';
+import React from "react";
+import Logo from "../../../assets/web-logo.png";
+import { AiOutlineMenu } from "react-icons/ai";
 const NavigationBar = () => {
-    return (
-        <div className="navbar container shadow-md rounded-md mx-auto bg-base-100">
+  return (
+      <div className="navbar container shadow-md rounded-md mx-auto bg-base-100">
         <div className="flex-1">
-          <span className='flex items-center '> <img className='w-16 me-3' src={Logo} alt="" /> <span className='text-red-500 font-bold text-2xl'>MAA CROCKERIES</span> </span>
+          <span className="flex items-center ">
+            {" "}
+            <img className="w-8 md:w-16 me-2 md:me-3" src={Logo} alt="" />{" "}
+            <span className="text-red-500 font-bold text-lg md:text-2xl">
+              MAA CROCKERIES
+            </span>{" "}
+          </span>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li tabIndex={0}>
-              <a>
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-              </a>
-              <ul className="p-2 bg-base-100">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
+        
+        <div className="hidden md:flex flex-none">
+          <ul className="menu font-bold menu-horizontal px-1">
+            <li>
+              <a>Home</a>
             </li>
-            <li><a>Item 3</a></li>
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Login</a>
+            </li>
+            <li>
+              <a>Register</a>
+            </li>
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10  avatar online">
+                  <img
+                    className="rounded-full border-green-400 border-2"
+                    src="https://i.ibb.co/pwyjxW5/clint3.jpg"
+                  />
+                </div>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+            
           </ul>
+          
         </div>
+
+        <div className=" sm:visible md:hidden dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <AiOutlineMenu className="text-red-600 text-xl"> </AiOutlineMenu>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+
+        
       </div>
-    );
+
+  );
 };
 
 export default NavigationBar;
