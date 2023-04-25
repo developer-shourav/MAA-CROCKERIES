49c3b133110cd8a466c3ab6c4783069fff868283
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-25 14:31:29 +0600

    primary footer added

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import Logo from "../../../assets/web-logo.png";
 const Footer = () => {
     return (
-        <footer  style={{backgroundColor:'#1b1b1b'}} className='py-10 text-white list-none'>
+        <footer   style={{backgroundColor:'#1b1b1b', color:'#8d8d8d'}} className='py-10  list-none'>
 
             <div className="container mx-auto">
                <div className='grid grid-cols-2 md:grid-cols-4'>
                
 
                 <div>
-                    <h4>About Us</h4>
+                    <h4 className='text-white text-lg font-bold'>About Us</h4>
                     <li>Inception</li>
                     <li>Milestones</li>
                     <li>Factories</li>
                 </div>
 
                 <div>
-                    <h4>BUSINESSES</h4>
+                <h4 className='text-white text-lg font-bold'>BUSINESSES</h4>
                     <li>CATEGORY & PRODUCT</li>
                     <li>Brands</li>
                     <li>Export</li>
                 </div>
 
                 <div>
-                    <h4>Legal</h4>
+                <h4 className='text-white text-lg font-bold'>Legal</h4>
                     <li>Privacy</li>
                     <li>Terms and Conditions</li>
                     <li>FAQ</li>
@@ -43,8 +43,13 @@ const Footer = () => {
             
             <hr style={{borderTop:'1px solid #383838'}}   />
 
-            <div className="container mx-auto flex justify-between items-center">
-                <div></div>
+            <div  className="container py-6 mt-2  mx-auto flex justify-between items-center">
+                <div className=''>
+                    <span>Terms of Use</span>
+                    <span className='mx-6'>Privacy</span>
+                    <span className='me-6'>About</span>
+                    <span>Contact</span>
+                </div>
 
                 <div>
                     <span>© 2023 MAA CROCKERIES | All rights reserved</span>
@@ -57,8 +62,3 @@ const Footer = () => {
 
 export default Footer;
 
-
-/* 
-li color: #8d8d8d 
-
-*/
