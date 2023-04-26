7cb184c5f13aa7fb5de28af697c6ceb528f5c53f
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-26 20:09:36 +0600

    contact added

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import ProfileImg from '../../assets/profile.jpg'
+import ProfileImg2 from '../../assets/profile2.png'
 const Contact = () => {
     return (
-        <div>
-            <h2>This is contact</h2>
+        <div className='container mx-auto'>
+
+            <div className='flex mt-20 mx-20 items-start justify-start'>
+               <div className='w-1/2'>
+               <img className="mask mask-squircle w-3/4 " src={ProfileImg} />
+               </div>
+
+               <div>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, hic.</p>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, hic.</p>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, hic.</p>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, hic.</p>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, hic.</p>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, hic.</p>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, hic.</p>
+               </div>
+            </div>
         </div>
     );
 };
