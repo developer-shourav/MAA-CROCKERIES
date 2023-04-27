f65a964feb24f13a141e3515cda0240adf6896ef
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-27 22:35:17 +0600

    google button added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BsKeyFill } from "react-icons/bs";
 import LoginImg from "../../assets/svg/login.svg";
 import { RiShieldKeyholeFill } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+import GoogleImg from '../../assets/icons/goole.png';
 const Login = () => {
     return (
         <div className='container mx-auto'>
@@ -30,13 +32,17 @@ const Login = () => {
           </label>
           <input type="text" placeholder="password" className="input input-bordered" />
           <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            <small>New to Ma Crockeries? <Link to="/register" className='text-violet-600 font-bold underline'> Register</Link></small>
           </label>
         </div>
         <div className="form-control mt-6">
           <button  style={{ background: "red" }}
               className="btn rounded border-0 shadow-red-500 shadow-md"> <BsKeyFill className='me-1 text-xl'/> Login </button>
         </div>
+        <div className='flex justify-center items-center mt-2'> 
+          <hr className='inline-block w-full border-1 border-red-400' /> <span className='mx-3 font-bold'> Or </span> <hr className='inline-block border-1 w-full border-red-400' />
+        </div>
+        <button className='btn normal-case bg-white   text-black rounded hover:bg-slate-100 google-btn-shadow border-0'> <img className='w-6 me-2' src={GoogleImg} alt="" /> Login with Google</button>
       </div>
     </div>
         </div>
