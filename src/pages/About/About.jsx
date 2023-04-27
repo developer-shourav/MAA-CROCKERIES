a839bf15b2b5267d2f3e8bd3277b150fe1a0748c
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-28 00:13:02 +0600

    about page added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import AboutImg from '../../assets/about.jpg';
 const About = () => {
-    return (
-        <div>
-            <h2>This is About</h2>
+  return (
+    <div
+      className="hero min-h-screen container mx-auto rounded-lg mt-10"
+      style={{
+        backgroundImage: `url("${AboutImg}")`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
