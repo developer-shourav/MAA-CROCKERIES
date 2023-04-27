558841f879210f70a5959973e2935fc94e825d42
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-27 21:38:55 +0600

    login page complete

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,61 +1,44 @@
 import React from 'react';
-import { IoIosSend } from "react-icons/io";
+import { BsKeyFill } from "react-icons/bs";
 import LoginImg from "../../assets/svg/login.svg";
-import { MdEmail } from "react-icons/md";
+import { RiShieldKeyholeFill } from "react-icons/ri";
 const Login = () => {
     return (
         <div className='container mx-auto'>
              <div className="w-11/12 mx-auto">
-      <h2 className="text-3xl font-bold text-center my-10">
+      <h2 className="text-3xl font-bold text-center mt-10">
         {" "}
-        <MdEmail className="text-red-600 text-5xl md:text-4xl lg:text-6xl inline" /> Send Mail
+        <RiShieldKeyholeFill className="text-red-600 text-5xl md:text-4xl lg:text-6xl inline" /> Please Login
       </h2>
-      <div className="flex flex-col md:flex-row justify-between gap-10 items-center">
+      <div className="flex flex-col md:flex-row justify-between  items-center">
         <div className="md:w-full">
           <img className="w-full" src={LoginImg} alt="" />
         </div>
 
         <div className="md:w-10/12">
-          <form action="">
-            <input
-              className="w-full outline-none border-2 rounded ps-2 py-2 bg-red-50 my-1  lg:my-2  border-red-500 "
-              type="text"
-              name="name"
-              placeholder="Your Name*"
-              required
-            />
-            <input
-              className="w-full outline-none border-2 rounded ps-2 py-2 bg-red-50 my-1 lg:my-2  border-red-500 "
-              type="email"
-              name="email"
-              placeholder="Your Email*"
-              required
-            />
-            <input
-              className="w-full outline-none border-2 rounded ps-2 py-2 bg-red-50 my-1 lg:my-2  border-red-500 "
-              type="text"
-              name="contact"
-              placeholder="Contact Info*"
-              required
-            />
-            <textarea
-              className="w-full outline-none border-2 rounded ps-2 py-2 bg-red-50 my-1 lg:my-2  border-red-500 "
-              name=""
-              id=""
-              cols="30"
-              rows="5"
-              placeholder="Your Message*"
-              required
-            ></textarea>
-            <button
-              type="submit"
-              style={{ background: "red" }}
-              className="btn rounded border-0 shadow-red-500 shadow-md"
-            >
-              <IoIosSend className="text-xl" />{" "}
-              <input type="submit" value="Send Message" />
-            </button>
-          </form>
+        <div className="card flex-shrink-0 w-full rounded-lg max-w-sm shadow-stone-400 shadow-md  bg-base-100">
+      <div className="card-body p-5">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="password" className="input input-bordered" />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button  style={{ background: "red" }}
+              className="btn rounded border-0 shadow-red-500 shadow-md"> <BsKeyFill className='me-1 text-xl'/> Login </button>
+        </div>
+      </div>
+    </div>
         </div>
       </div>
     </div>
