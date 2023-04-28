f967c232f55424a75c2ee0c5e26b68c454a51a88
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-28 11:06:05 +0600

    styled added for big screen

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         About Us
       </h2>
        <div className="flex items-center justify-between">
-         <div className="w-1/2" >
+         <div className="w-1/2 lg:ms-40 " >
          <h1 className='font-bold text-3xl md:text-5xl my-2 md:mb-5'> Chose Our Top </h1>
                 <h1 className='font-bold text-2xl  md:text-4xl lg:text-6xl mb-5'> Crockeries Here </h1>
                 
@@ -16,23 +16,11 @@ const About = () => {
                 <p className='md:w-3/4 text-gray-700 mb-5'>We stands for quality, authenticity, trust and satisfaction. We have a massive collection of products and brands that you will love.</p>
                 <button style={{background:'red'}} className='btn border-0 rounded-none px-8 '> Shop Now </button>
 
-                 <div className=' mt-5 md:mt-10 font-bold flex gap-20'>
-
-                   <div>
-                     <h3 className=' text-2xl md:text-3xl'>1534+</h3>
-                     <p ><small>Total Products</small></p>
-                   </div>
-
-                   <div>
-                   <h3 className=' text-2xl md:text-3xl'>1000+</h3>
-                     <p><small>Monthly Customers</small></p>
-                   </div>
-
-                 </div>
+                
          </div>
 
          <div className="w-1/2">
-           <img className="mx-auto "  src={AboutImg} alt="" />
+           <img className="mx-auto"  src={AboutImg} alt="" />
          </div>
 
        </div>
