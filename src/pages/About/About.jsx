9673a843a602f780c5d539c904edda632a5483db
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-28 10:52:27 +0600

    about section markup added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,15 +1,38 @@
 import React from "react";
-import AboutImg from '../../assets/about.jpg';
+import AboutImg from '../../assets/about.png';
 const About = () => {
   return (
     <div className="container mx-auto">
-       <div className="grid grid-cols-2 items-center">
-         <div>
-          <img src={AboutImg} alt="" />
+       <h2 className="text-4xl font-bold text-center my-10">
+        {" "}
+        About Us
+      </h2>
+       <div className="flex items-center justify-between">
+         <div className="w-1/2" >
+         <h1 className='font-bold text-3xl md:text-5xl my-2 md:mb-5'> Chose Our Top </h1>
+                <h1 className='font-bold text-2xl  md:text-4xl lg:text-6xl mb-5'> Crockeries Here </h1>
+                
+                
+                <p className='md:w-3/4 text-gray-700 mb-5'>We stands for quality, authenticity, trust and satisfaction. We have a massive collection of products and brands that you will love.</p>
+                <button style={{background:'red'}} className='btn border-0 rounded-none px-8 '> Shop Now </button>
+
+                 <div className=' mt-5 md:mt-10 font-bold flex gap-20'>
+
+                   <div>
+                     <h3 className=' text-2xl md:text-3xl'>1534+</h3>
+                     <p ><small>Total Products</small></p>
+                   </div>
+
+                   <div>
+                   <h3 className=' text-2xl md:text-3xl'>1000+</h3>
+                     <p><small>Monthly Customers</small></p>
+                   </div>
+
+                 </div>
          </div>
 
-         <div>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio minima quibusdam modi architecto, alias laudantium repellendus, maxime quaerat eius quas? Illum eveniet enim unde quam laboriosam. Enim laboriosam officiis et magnam temporibus facilis voluptas quisquam maiores reprehenderit quae quia recusandae, repellat odio molestias eum distinctio aliquam? Explicabo alias adipisci esse pariatur consequuntur, blanditiis nam ea! Dolor, voluptatibus possimus dolorem soluta enim aspernatur dolore repudiandae aliquam maxime quae voluptatem aliquid, est ipsa veniam assumenda molestias ipsam fugiat incidunt minima. Optio nesciunt a, amet recusandae accusamus iure? Autem nulla ipsum temporibus laudantium harum minus dignissimos corrupti. Quis possimus similique voluptatem doloribus.</p>
+         <div className="w-1/2">
+           <img className="mx-auto "  src={AboutImg} alt="" />
          </div>
 
        </div>
