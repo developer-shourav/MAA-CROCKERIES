83775bc9a9adcf565fc13fb280ffab94ecc38f79
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-28 12:03:29 +0600

    large and medium about complete

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,29 +1,66 @@
 import React from "react";
-import AboutImg from '../../assets/about.png';
+import AboutImg from "../../assets/about.png";
 const About = () => {
   return (
-    <div className="container mx-auto">
-       <h2 className="text-4xl font-bold text-center my-10">
-        {" "}
-        About Us
-      </h2>
-       <div className="flex flex-col-reverse md:flex-row gap-8 items-center justify-between">
-         <div className="md:w-1/2 px-5 md:ms-20 lg:ms-40 " >
-         <h1 className='font-bold text-3xl md:text-5xl lg:text-5xl my-2 md:mb-5'> We Are Your Mate </h1>
-                <h1 className='font-bold text-2xl  md:text-4xl  mb-5'> For Providing You The Best Products </h1>
-                
-                
-                <p className='md:w-3/4 text-gray-700 mb-5'>We stands for quality, authenticity, trust and satisfaction. We have a massive collection of products and brands that you will love.</p>
-                <button style={{background:'red'}} className='btn border-0 rounded-none px-8 '> Shop Now </button>
+    <div className="">
+      <h2 className="text-4xl font-bold text-center my-10"> About Us</h2>
+      <div className="flex flex-col-reverse md:flex-row gap-8 items-center justify-between container mx-auto">
+        <div className="md:w-1/2 px-5 md:ms-10 lg:ms-40 ">
+          <h1 className="font-bold text-3xl md:text-5xl lg:text-5xl my-2 md:mb-5">
+            {" "}
+            We Are Your Mate{" "}
+          </h1>
+          <h1 className="font-bold text-2xl  md:text-4xl  mb-5">
+            {" "}
+            For Providing You The Best Products{" "}
+          </h1>
 
-                
-         </div>
+          <p className="lg:w-3/4  text-gray-700 mb-5">
+            We stands for quality, authenticity, trust and satisfaction. We have
+            a massive collection of products and brands that you will love.
+          </p>
+          <button
+            style={{ background: "red" }}
+            className="btn border-0 rounded-none px-8 "
+          >
+            {" "}
+            Shop Now{" "}
+          </button>
+        </div>
 
-         <div className="md:w-1/2">
-           <img className="mx-auto w-9/12 lg:w-4/5 px-10"  src={AboutImg} alt="" />
-         </div>
+        <div className="md:w-1/2">
+          <img
+            className="mx-auto w-9/12 md:w-10/12 lg:w-4/5 px-10  md:px-0 lg:px-10"
+            src={AboutImg}
+            alt=""
+          />
+        </div>
+      </div>
 
-       </div>
+      <div className=" md:py-6 lg:py-10 about-bg my-5">
+
+        <div className="container mx-auto flex items-center justify-evenly text-center font-bold ">
+          <div>
+            <h3 className="md:text-3xl lg:text-4xl">1000+</h3>
+            <p>Product Items</p>
+          </div>
+
+          <div className="">
+          <h3 className="md:text-3xl lg:text-4xl">20+</h3>
+            <p>Popular Brands</p>
+          </div>
+
+          <div>
+          <h3 className="md:text-3xl lg:text-4xl">25 Years</h3>
+            <p>Business Experience</p>
+          </div>
+
+          <div>
+            <h3 className="md:text-3xl lg:text-4xl">90%</h3>
+            <p>Return Customers</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
