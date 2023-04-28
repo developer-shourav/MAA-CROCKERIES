0436481247e2babdd82c59d4a433d9f7c757188b
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-28 11:29:44 +0600

    primary about section complete

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,10 +7,10 @@ const About = () => {
         {" "}
         About Us
       </h2>
-       <div className="flex items-center justify-between">
-         <div className="w-1/2 lg:ms-40 " >
-         <h1 className='font-bold text-3xl md:text-5xl my-2 md:mb-5'> Chose Our Top </h1>
-                <h1 className='font-bold text-2xl  md:text-4xl lg:text-6xl mb-5'> Crockeries Here </h1>
+       <div className="flex flex-col-reverse md:flex-row gap-8 items-center justify-between">
+         <div className="md:w-1/2 px-5 md:ms-20 lg:ms-40 " >
+         <h1 className='font-bold text-3xl md:text-5xl lg:text-5xl my-2 md:mb-5'> We Are Your Mate </h1>
+                <h1 className='font-bold text-2xl  md:text-4xl  mb-5'> For Providing You The Best Products </h1>
                 
                 
                 <p className='md:w-3/4 text-gray-700 mb-5'>We stands for quality, authenticity, trust and satisfaction. We have a massive collection of products and brands that you will love.</p>
@@ -19,8 +19,8 @@ const About = () => {
                 
          </div>
 
-         <div className="w-1/2">
-           <img className="mx-auto"  src={AboutImg} alt="" />
+         <div className="md:w-1/2">
+           <img className="mx-auto w-9/12 lg:w-4/5 px-10"  src={AboutImg} alt="" />
          </div>
 
        </div>
